feea62802254a92f3c56d85a87fbab96a80ab94f
Fabiane Albuquerque <bii.nett@gmail.com>
2019-02-18 16:00:07 -0300

    Criando html, css e pastas base

---
+++ src/script.js
@@ -0,0 +1,63 @@
+
+
+
+function twittar(){
+
+    let textTwitter= document.querySelector(".write-text").value;
+
+
+
+
+
+
+
+
+    
+    return document.write("Texto digitado: " + textTwitter)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var $wrapper = document.querySelector('.wrapper'),
+    // Pega a string do conteúdo atual
+    HTMLTemporario = $wrapper.innerHTML,
+    // Novo HTML que será inserido
+    HTMLNovo = 'Teste prepend com innerHTML </br>';
+
+// Concatena as strings colocando o novoHTML antes do HTMLTemporario
+HTMLTemporario = HTMLNovo + HTMLTemporario;
+
+// Coloca a nova string(que é o HTML) no DOM
+$wrapper.innerHTML = HTMLTemporario; */
